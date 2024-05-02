0c981af0b7f69a413fb9ddfc35428ffc3034af8b
Ssoyeonnnnn <psyeon3188@gmail.com>
2024-05-02 10:36:24 +0900

    동일한 사용자가 있다면 error 메세지 띄우기

--- src/main/java/com/example/myBoard/controller/UserAccountController.java
+++ src/main/java/com/example/myBoard/controller/UserAccountController.java
@@ -3,6 +3,7 @@ package com.example.myBoard.controller;
 import com.example.myBoard.dto.UserCreateForm;
 import com.example.myBoard.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,8 +36,16 @@ public class UserAccountController {
         if (bindingResult.hasErrors()) {
             return "signup";
         }
-        userService.createUser(userCreateForm);
-
+        try {
+            userService.createUser(userCreateForm);
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자 입니다.");
+            return "signup";
+        } catch (Exception e){
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup";
+        }
         return "redirect:/";
     }
 }
